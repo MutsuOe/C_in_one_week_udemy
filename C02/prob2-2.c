6dd590dc79e339277c48e30674eb84979a92c004
mutsuoe <mutsuoe@outlook.jp>
2025-08-23 17:31:22 +0900

    3

---
+++ C02/prob2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int w, h;
+    printf("長方形の幅：");
+    scanf("%d", &w);
+    printf("長方形の高さ");
+    scanf("%d", &h);
+    printf("長方形の面積は，%dm2です。\n", w * h);
+
+}
