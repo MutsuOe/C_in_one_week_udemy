6c37ec8a5c00a724dec8abacde07faf1c868ed7d
mutsuoe <mutsuoe@outlook.jp>
2025-08-23 17:54:11 +0900

    4

---
+++ C03/3-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("１から３の値を入力してください：");
+    scanf("%d", &num);
+
+    if(num == 1){
+        printf("one\n");
+    }else if(num == 2){
+        printf("two\n");
+    }else if(num == 3){
+        printf("three\n");
+    }else{
+        printf("不適切な値です\n");
+    }
+}
---
+++ C03/3-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int dice;
+    printf("1から6の数値を入れて：");
+    scanf("%d", &dice);
+
+    if ( 1 <= dice && dice <= 6 ) {
+        if(dice == 2 || dice == 4 || dice == 6){
+            printf("丁（チョウ）です。\n");
+        }else{
+            printf("半（ハン）です。\n");
+        }
+    }
+    else{
+        printf("範囲外の数値です。\n");
+    }
+}
